36ef429e1a202dc05c9c30ef4c9119a35b92fff5
ASHUZA <ashuzakasumbepascal19@gmail.com>
2021-08-24 14:37:21 -0100

    add donwload resume fonctionality

--- src/Section/Cover.jsx
+++ src/Section/Cover.jsx
@@ -1,32 +1,31 @@
-import React from 'react';
+import React from "react";
+import CV_PDF from "./../CV/ASHUZA_KASUMBE_PASCAL_CV.pdf";
 
-
-const Cover =()=> {
+const Cover = () => {
   return (
-      <>
-        <section id="cover" className="d-flex align-items-center Cover">
-        <div className="container position-relative" data-aos="fade-up" data-aos-delay="500">
+    <>
+      <section id="cover" className="d-flex align-items-center Cover">
+        <div
+          className="container position-relative"
+          data-aos="fade-up"
+          data-aos-delay="500"
+        >
+          <div className="title_container">
+            <h1>ASHUZA PASCAL</h1>
 
-
-
-            <div className="title_container">
-
-                <h1>ASHUZA PASCAL</h1>
-
-                <div className="title_animation">
-                    <div className="title_detail">UI/UX Designer</div>
-                    <div className="title_detail">
-                        <span> Fulls stack Web Developer</span>
-                    </div>
-                </div>
-                <a href="#about" className="btn-get-started scrollto">Download CV</a>
-                </div>
-                </div>
-
-
-
-    </section>
-      </>
+            <div className="title_animation">
+              <div className="title_detail">UI/UX Designer</div>
+              <div className="title_detail">
+                <span> Fulls stack Web Developer</span>
+              </div>
+            </div>
+            <a href={CV_PDF} download="MyCV" target="_blank">
+              <p className="btn-get-started scrollto">Download CV</p>
+            </a>
+          </div>
+        </div>
+      </section>
+    </>
   );
-}
-export default Cover
+};
+export default Cover;
